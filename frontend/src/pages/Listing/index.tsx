150ced063523f60d08bb07426e1a1097b1bf4de1
Victor Rodrigues <aniebson@gmail.com>
2022-01-11 22:06:09 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,31 @@
-import Pagination from "components/Pagination";
-function Listing() {
+import Pagination from 'components/Pagination'
+import MovieCard from 'components/MovieCard'
 
+function Listing() {
   return (
-    <Pagination />
-  );
+    <>
+      <Pagination />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
-export default Listing;
+export default Listing
